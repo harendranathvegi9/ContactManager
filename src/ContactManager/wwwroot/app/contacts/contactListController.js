67d6cac7216be436dfb45230447b30b3f0df5414
vominhtri1107 <vominhtri1107@gmail.com>
2016-09-04 15:43:53 +0700

    Adding delete contact function
    Adding delete contact function
    Make the link to website clickable on contact detail screen

--- src/ContactManager/wwwroot/app/contacts/contactListController.js
+++ src/ContactManager/wwwroot/app/contacts/contactListController.js
@@ -5,8 +5,40 @@
         .module("contactManager")
         .controller("contactListController", contactListController);
 
-    function contactListController($scope, contactResource) {
-        $scope.contacts = contactResource.query();
+    function contactListController($scope, $state, contactResource) {
+        loadContactList();
+
+        $scope.showDeleteContactConfirm = function (contactId) {
+            storeContactIdForDelete(contactId);
+            openDeleteContactConfirmModal();
+        };
+
+        $scope.deleteContact = function () {
+            var contactId = getContactIdToDelete();
+            contactResource.delete({ contactId: contactId });
+            closeDeleteContactConfirmModal();
+            loadContactList();
+        }
+
+        function loadContactList() {
+            $scope.contacts = contactResource.query();
+        }
+
+        function storeContactIdForDelete(contactId) {
+            $("#deleteContactButton").data("contactId", contactId);
+        }
+
+        function getContactIdToDelete() {
+            return $("#deleteContactButton").data("contactId");
+        }
+
+        function openDeleteContactConfirmModal() {
+            $("#deleteConfirmModal").modal();
+        }
+
+        function closeDeleteContactConfirmModal() {
+            $("#deleteConfirmModal").modal("hide");
+        }
     }
 
 }());
